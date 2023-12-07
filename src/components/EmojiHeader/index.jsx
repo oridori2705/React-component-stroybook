771450365d4d20386d606e7364a3c432078e423b
신준혁 <oridori2705@naver.com>
2023-12-07 18:19:46 +0900

    commit: 이모지 검색 헤더 컴포넌트 구현

---
+++ src/components/EmojiHeader/index.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+const EmojiHeading = styled.h1`
+  margin: 0;
+  font-size: 32px;
+  text-align: center;
+`
+
+const EmojiHeader = () => {
+  return <EmojiHeading>Emoji Search</EmojiHeading>
+}
+
+export default EmojiHeader
