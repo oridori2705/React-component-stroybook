1859c19c4d5c3c454101f053bd3cae35f8b78f04
신준혁 <oridori2705@naver.com>
2023-12-07 18:50:19 +0900

    commit: 이모지 컴포넌트 조합

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react'
+import EmojiHeader from './components/EmojiHeader'
+import SearchBox from './components/EmojiSearchBox'
+import emojiJson from './data/emoji.json'
+import EmojiList from './components/EmojiList'
+
 function App() {
-  return <></>
+  const [keyword, setKeyword] = useState('')
+
+  return (
+    <div>
+      <EmojiHeader />
+      <SearchBox onSearch={setKeyword} />
+      <EmojiList
+        emojis={emojiJson}
+        keyword={keyword}
+      />
+    </div>
+  )
 }
 
 export default App
