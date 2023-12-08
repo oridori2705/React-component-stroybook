42843313221109120d729da1bd5d7f2abcb97361
신준혁 <oridori2705@naver.com>
2023-12-08 18:24:23 +0900

    commit: Upload 컴포넌트 - Drag&Drop 기능 구현

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -6,8 +6,17 @@ const Input = styled.input`
   display: none;
 `
 
-const Upload = ({ children, name, accept, value, onChange, ...props }) => {
+const Upload = ({
+  children,
+  droppable,
+  name,
+  accept,
+  value,
+  onChange,
+  ...props
+}) => {
   const [file, setFile] = useState(value)
+  const [dragging, setDragging] = useState(false)
 
   //파일 업로드 input태그를 꾸미기 위해서는
   //보이지 않게 한 뒤 useRef로 input태그에 접근한다.
@@ -22,9 +31,49 @@ const Upload = ({ children, name, accept, value, onChange, ...props }) => {
     setFile(changedFile)
     onChange && onChange(changedFile)
   }
+
+  const handleDragEnter = e => {
+    if (!droppable) return
+
+    e.preventDefault() // 브라우저 기본이벤트 막기
+    e.stopPropagation() // 이벤트 전파 막기
+
+    //e.dataTransfer.items : 드래그한 요소가 있을 때
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setDragging(true)
+    }
+  }
+  const handleDragLeave = e => {
+    if (!droppable) return
+    e.preventDefault()
+    e.stopPropagation()
+    setDragging(false)
+  }
+  // 이벤트 전파를 막기위한 handleDragover함수
+  const handleDragOver = e => {
+    if (!droppable) return
+    e.preventDefault()
+    e.stopPropagation()
+  }
+  const handleFileDrop = e => {
+    if (!droppable) return
+    e.preventDefault()
+    e.stopPropagation()
+
+    const files = e.dataTransfer.files
+    const changeFile = files[0]
+    setFile(changeFile)
+    onChange && onChange(changeFile)
+    setDragging(false)
+  }
+
   return (
     <div
       onClick={handleChooseFile}
+      onDrop={handleFileDrop}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
       {...props}>
       <Input
         ref={inputRef}
@@ -37,7 +86,7 @@ const Upload = ({ children, name, accept, value, onChange, ...props }) => {
           아래와 같이
           자식요소가 jsx 반환하는 함수일 경우 함수에 file을 인수로 넣어서 보내주고, 아니라면 node인 children을 보여준다.
           */}
-      {typeof children === 'function' ? children(file) : children}
+      {typeof children === 'function' ? children(file, dragging) : children}
     </div>
   )
 }
@@ -45,6 +94,7 @@ const Upload = ({ children, name, accept, value, onChange, ...props }) => {
 Upload.propTypes = {
   children: PropTypes.node.isRequired,
   name: PropTypes.string,
+  droppable: PropTypes.bool,
   accept: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func
--- src/stories/Upload.stories.jsx
+++ src/stories/Upload.stories.jsx
@@ -10,3 +10,21 @@ export const Default = () => {
     <Upload>{file => <button>{file ? file.name : 'Click me'}</button>}</Upload>
   )
 }
+
+export const Droppable = () => {
+  return (
+    <Upload droppable>
+      {(file, dragging) => (
+        <div
+          style={{
+            width: 300,
+            height: 100,
+            border: '4px dashed #aaa',
+            borderColor: dragging ? 'black' : '#aaa'
+          }}>
+          {file ? file.name : 'Click or drag file to this area to upload'}
+        </div>
+      )}
+    </Upload>
+  )
+}
