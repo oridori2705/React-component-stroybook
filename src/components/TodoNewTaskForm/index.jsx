9e93e75dfe042413b9605bbf1852b77c9f23e87d
신준혁 <oridori2705@naver.com>
2023-12-08 14:49:56 +0900

    commit: Todo 앱 - Form 컴포넌트 구현

---
+++ src/components/TodoNewTaskForm/index.jsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled'
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const Form = styled.form`
+  width: 400px;
+`
+
+const Input = styled.input`
+  width: 332px;
+  height: 32px;
+  padding: 4px 6px;
+  border-radius: 8px;
+  border: 2px solid;
+  box-sizing: border-box;
+`
+
+const SubmitButton = styled.button`
+  width: 60px;
+  height: 32px;
+  padding: 4px 6px;
+  margin-left: 8px;
+  color: white;
+  border-radius: 8px;
+  border: none;
+  background-color: black;
+  box-sizing: border-box;
+  cursor: pointer;
+`
+
+const TodoNewTaskForm = props => {
+  const [task, setTask] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault()
+    setTask('')
+  }
+  return (
+    <Form
+      {...props}
+      onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        value={task}
+        onChange={e => setTask(e.target.value)}
+        required
+      />
+      <SubmitButton>Add</SubmitButton>
+    </Form>
+  )
+}
+
+export default TodoNewTaskForm
---
+++ src/stories/TodoNewTaskForm.stories.jsx
@@ -0,0 +1,9 @@
+import TodoNewTaskForm from '../components/TodoNewTaskForm'
+
+export default {
+  title: 'Component/TodoNewTaskForm',
+  component: TodoNewTaskForm
+}
+export const Default = () => {
+  return <TodoNewTaskForm />
+}
