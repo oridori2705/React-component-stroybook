33bdb31b5f8074be2653affa78c8aa8de51b8ba0
신준혁 <oridori2705@naver.com>
2023-12-08 14:50:43 +0900

    commit: Todo 앱 - 컴포넌트 병합

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import styled from '@emotion/styled'
+import Header from './components/Header'
+import TodoNewTaskForm from './components/TodoNewTaskForm'
+import TodoTaskList from './components/TodoTaskList'
+
+const Container = styled.div`
+  width: 400px;
+  margin: 0 auto;
+`
+
 function App() {
-  return <div></div>
+  return (
+    <Container>
+      <Header>Todo</Header>
+      <TodoNewTaskForm />
+      <TodoTaskList css={{ marginTop: 16 }} />
+    </Container>
+  )
 }
 
 export default App
