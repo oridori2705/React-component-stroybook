326216a9ee6aca0c108018fc14fb67476c53311d
신준혁 <oridori2705@naver.com>
2023-12-10 22:22:19 +0900

    commit: Skeleton 컴포넌트 구현 - 애니메이션 스타일링

---
+++ src/components/Skeleton/Base.jsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled'
+
+const Base = styled.div`
+  display: inline-block;
+  border-radius: 4px;
+  background-image: linear-gradient(
+    90deg,
+    #dfe3e8 0px,
+    #efefef 40px,
+    #dfe3e8 80px
+  );
+  background-size: 200% 100%;
+  background-position: 0 center;
+  animation:
+    skeleton--zoom-in 0.2s ease-out,
+    skeleton--loading 2s infinite linear;
+  // 없어졌다가 나타나는 효과
+  @keyframes skeleton--zoom-in {
+    0% {
+      opacity: 0;
+      transform: scale(0.95);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  // 왼쪽에서 오른쪽으로 가는 효과 - 로딩이 천천히 가도록 50%에서 -100%을 적용해 천천히 반복됨
+  @keyframes skeleton--loading {
+    0% {
+      background-position-x: 100%;
+    }
+    50% {
+      background-position-x: -100%;
+    }
+    100% {
+      background-position-x: -100%;
+    }
+  }
+`
+
+export default Base
---
+++ src/stories/Skeleton.stories.jsx
@@ -0,0 +1,67 @@
+import Skeleton from '../components/Skeleton'
+
+export default {
+  title: 'Component/Skeleton'
+}
+
+export const Box = args => <Skeleton.Box {...args} />
+Box.argTypes = {
+  width: { control: 'number' },
+  height: { control: 'number' }
+}
+Box.args = {
+  width: 200,
+  height: 100
+}
+
+export const Circle = args => <Skeleton.Circle {...args} />
+Circle.argTypes = {
+  size: { control: 'number' }
+}
+Circle.args = {
+  size: 100
+}
+
+export const Paragraph = args => <Skeleton.Paragraph {...args} />
+Paragraph.argTypes = {
+  line: { control: 'number' }
+}
+Paragraph.args = {
+  line: 3
+}
+
+export const Sample = () => {
+  return (
+    <>
+      <div>
+        <div style={{ float: 'left', marginRight: 16 }}>
+          <Skeleton.Circle size={60} />
+        </div>
+        <div style={{ float: 'left', width: '80%' }}>
+          <Skeleton.Paragraph line={4} />
+        </div>
+        <div style={{ clear: 'both' }} />
+      </div>
+
+      <div style={{ marginTop: 16 }}>
+        <div style={{ float: 'left', marginRight: 16 }}>
+          <Skeleton.Circle size={60} />
+        </div>
+        <div style={{ float: 'left', width: '80%' }}>
+          <Skeleton.Paragraph line={4} />
+        </div>
+        <div style={{ clear: 'both' }} />
+      </div>
+
+      <div style={{ marginTop: 16 }}>
+        <div style={{ float: 'left', marginRight: 16 }}>
+          <Skeleton.Circle size={60} />
+        </div>
+        <div style={{ float: 'left', width: '80%' }}>
+          <Skeleton.Paragraph line={4} />
+        </div>
+        <div style={{ clear: 'both' }} />
+      </div>
+    </>
+  )
+}
