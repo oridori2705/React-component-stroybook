ee011b6c6e667fa461d69ce1abc679192e2e827e
신준혁 <oridori2705@naver.com>
2023-12-11 16:14:53 +0900

    fix: Provider 주석 추가

--- src/contexts/TaskProvider.jsx
+++ src/contexts/TaskProvider.jsx
@@ -3,6 +3,7 @@ import { v4 } from 'uuid' //랜덤한 id를 만들어줌
 import PropTypes from 'prop-types'
 import useLocalStorage from '../hooks/useLocalStorage'
 
+// 컨텍스트 만들기 - 컨텐스트의 내용을 provider을 만들어 채움
 const TaskContext = createContext()
 
 //컨슈머 - 편하게 사용하기위해 커스텀 훅처럼 작성, 원래는 컴포넌트마다 아래와 같이 호출하면됨
@@ -32,6 +33,7 @@ const TaskProvider = ({ children }) => {
     setTasks(tasks.filter(item => item.id !== id))
   }
   return (
+    //vaule의 값들이 나중에 컨슈머가 받을 수 있는 데이터
     <TaskContext.Provider value={{ tasks, addTask, updateTask, removeTask }}>
       {children}
     </TaskContext.Provider>
