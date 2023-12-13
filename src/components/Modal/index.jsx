4affcc4203cf6aecf273a8e59366cc460bd61a88
신준혁 <oridori2705@naver.com>
2023-12-14 00:36:13 +0900

    commit: Modal 컴포넌트 구현 - React Portal, useClickAway hook 사용

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled'
+import { useEffect, useMemo } from 'react'
+import ReactDOM from 'react-dom'
+import useClickAway from '../../hooks/useClickAway'
+
+const BackgroundDim = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+`
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 8px;
+  background-color: white;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+`
+
+const Modal = ({
+  children,
+  width = 500,
+  height,
+  visible = false,
+  onClose,
+  ...props
+}) => {
+  //빈 곳 클릭시 모달 닫기 위해 useClickAway 사용
+  const ref = useClickAway(() => {
+    onClose && onClose()
+  })
+
+  const containerStyle = useMemo(
+    () => ({
+      width,
+      height
+    }),
+    [width, height]
+  )
+
+  //모달은 어떤 요소의 자식이 아니라 body요소의 자식이어야한다.
+  const el = useMemo(() => document.createElement('div'), [])
+  useEffect(() => {
+    document.body.appendChild(el)
+    return () => {
+      document.body.removeChild(el)
+    }
+  })
+  //React Portal을 이용해서 위에서 정의한 el에 styled컴포넌트로 만들어준 아래 DOM들을 넣어줘야한다.
+  return ReactDOM.createPortal(
+    <BackgroundDim style={{ display: visible ? 'block' : 'none' }}>
+      <ModalContainer
+        ref={ref}
+        {...props}
+        style={{ ...props.style, ...containerStyle }}>
+        {children}
+      </ModalContainer>
+    </BackgroundDim>,
+    el
+  )
+}
+
+export default Modal
--- src/hooks/useAsyncFn.jsx
+++ src/hooks/useAsyncFn.jsx
@@ -1,22 +1,30 @@
 import { useCallback, useRef, useState } from 'react'
 
+//비동기 로직을 제거하기 위해 사용되는 훅
 const useAsyncFn = (fn, deps) => {
+  // 만약 비동기 로직이 여러번 A요청과 B요청이 일어났을 때
+  // A요청이 늦는다면 state에는 늦은 A요청의 값이 최종적으로 할당된다.(B는 없어짐)
+  // 그래서 제일 마지막에 요청한 비동기 로직만 받을 수 있도록 할 것이다.
   const lastCallId = useRef(0)
+  // { 로딩 여부, 결과적으로 반환되는 값, 에러}
   const [state, setState] = useState({ isLoading: false })
 
   const callback = useCallback((...args) => {
+    // id값을 증가시켜서 비동기 요청에 id를 부여함
     const callId = ++lastCallId.current
-
+    //로딩중이 아닐 때 로딩으로 만들어줌
     if (!state.isLoading) {
       setState({ ...state, isLoading: true })
     }
-
+    //함수 호출 후 비동기로직이 끝나면
     return fn(...args).then(
       value => {
-        callId === lastCallId.current && setState({ value, isLoading: false })
+        // 비동기 로직 성공 시 그 값을 넣어줌
+        callId === lastCallId.current && setState({ value, isLoading: false }) // 마지막 요청인지 검사하고 state를 할당함
         return value
       },
       error => {
+        // 비동기 로직 실패 시 에러 값을 넣어줌
         callId === lastCallId.current && setState({ error, isLoading: false })
         return error
       }
