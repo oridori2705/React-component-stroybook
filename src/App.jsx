6100dd2f8621783d03d79f5d955490f8af1f0287
신준혁 <oridori2705@naver.com>
2023-12-15 00:34:21 +0900

    commit: axios, Context API 활용 게시글 앱 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+import axios from 'axios'
+import { useCallback } from 'react'
+import PostList from './components/domain/PostList'
+import PostProvider from './contexts/PostProvider'
+import { useAsync } from './hooks'
+import { Header } from './components'
+import PostAddForm from './components/domain/PostAddForm'
+
+const App = () => {
+  const initialPosts = useAsync(async () => {
+    return await axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.data)
+  }, [])
+
+  const handleAddPost = useCallback(async post => {
+    console.log(post)
+    return await axios
+      .post(`https://jsonplaceholder.typicode.com/posts`, post)
+      .then(response => response.data)
+  }, [])
+
+  const handleDeletePost = useCallback(async id => {
+    return await axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(() => ({ id }))
+  }, [])
+
+  return (
+    <PostProvider
+      initialPosts={initialPosts.value}
+      handleAddPost={handleAddPost}
+      handleDeletePost={handleDeletePost}>
+      <Header>Posts</Header>
+      <PostAddForm />
+      <PostList />
+    </PostProvider>
+  )
+}
+
+export default App
+
+/*
+//그림판 앱
 import { useState } from 'react'
 import Paint from './components/Paint'
 import { DashPlugin, EraserPlugin, PenPlugin } from './components/Paint/plugins'
@@ -36,6 +80,7 @@ const App = () => {
 }
 
 export default App
+*/
 
 /*
 // Todo 앱
---
+++ src/components/domain/PostItem.jsx
@@ -0,0 +1,37 @@
+import { useCallback, useState } from 'react'
+import { Header, Spinner, Text } from '..'
+import { usePostContext } from '../../contexts/PostProvider'
+
+const PostItem = ({ post }) => {
+  const [loading, setLoading] = useState(false)
+  const { onDeletePost } = usePostContext()
+
+  const handleDeletePost = useCallback(
+    async id => {
+      setLoading(true)
+      await onDeletePost(id)
+      setLoading(false)
+    },
+    [onDeletePost]
+  )
+
+  return (
+    <li>
+      <Header
+        level={3}
+        strong>
+        {post.title}
+      </Header>
+      <Text>{post.body}</Text>
+      <div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <button onClick={() => handleDeletePost(post.id)}>Delete</button>
+        )}
+      </div>
+    </li>
+  )
+}
+
+export default PostItem
